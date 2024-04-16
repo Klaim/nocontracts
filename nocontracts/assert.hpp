42278470ead5bea1cc0c50bd7507d5d60a1aacf9
Klaim (Joël Lamotte) <142265+Klaim@users.noreply.github.com>
2024-04-16 17:38:01 +0200

    Use std::format or std::print if available and allow user to specify which one to use (or C formatting) (#2)

--- nocontracts/assert.hpp
+++ nocontracts/assert.hpp
@@ -1,19 +1,72 @@
 #pragma once
 #include <cstdio>
-#include <cstdlib>
 #include <csignal>
 
 
 ///////////////////////////////////////////////////////////////////
 // Possible bits used to compose the behavior:
+
+// if not specified by the user but available, use std::print
+#if not defined(NOCONTRACTS_USE_STD_PRINT) and not defined(NOCONTRACTS_USE_STD_FORMAT)
+# if __cplusplus >= 202002L
+#  include <version>
+#  ifdef __cpp_lib_print
+#   define NOCONTRACTS_USE_STD_PRINT 1
+#  endif
+# endif
+#else
+// The option is defined, but is it supported? If not we fail.
+#  if defined(NOCONTRACTS_USE_STD_PRINT) and not defined(__cpp_lib_print)
+#    error "std::print usage is requested but not available"
+#  endif
+#endif
+
+// if not specified by the user but available and std::print is not already forced, use std::format
+#if not defined(NOCONTRACTS_USE_STD_FORMAT) and not defined(NOCONTRACTS_USE_STD_PRINT)
+# if __cplusplus >= 202002L
+#  include <version>
+#  ifdef __cpp_lib_format
+#   define NOCONTRACTS_USE_STD_FORMAT 1
+#  endif
+# endif
+// The option is defined, but is it supported? If not we fail.
+#  if defined(NOCONTRACTS_USE_STD_FORMAT) and not defined(__cpp_lib_format)
+#    error "std::format usage is requested but not available"
+#  endif
+#endif
+
+// user specified to not use either std::format or std::print but C's formatting
+#if defined(NOCONTRACTS_USE_CFORMAT) && NOCONTRACTS_USE_CFORMAT == 1
+# ifdef NOCONTRACTS_USE_STD_FORMAT
+#   undef NOCONTRACTS_USE_STD_FORMAT
+# endif
+# ifdef NOCONTRACTS_USE_STD_PRINT
+#   undef NOCONTRACTS_USE_STD_PRINT
+# endif
+#endif
+
 #ifndef NOCONTRACTS_LOG_FAILURE
-# define NOCONTRACTS_LOG_FAILURE( expr__, message__ ) \
-    fprintf(stderr, "Assertion Failed (%s:%i): %s - (%s is wrong)\n", __FILE__, __LINE__, message__, #expr__ );
+# if defined(NOCONTRACTS_USE_STD_PRINT) && NOCONTRACTS_USE_STD_PRINT == 1
+#   include <print>
+#   include <cstdio>
+#   define NOCONTRACTS_LOG_FAILURE( expr__, message__ ) \
+      ::std::print(stderr, "Assertion Failed ({}:{}): {} - ({} is wrong)\n", __FILE__, __LINE__, message__, #expr__ )
+# elif defined(NOCONTRACTS_USE_STD_FORMAT) && NOCONTRACTS_USE_STD_FORMAT == 1
+#   include <format>
+#   include <cstdio>
+#   define NOCONTRACTS_LOG_FAILURE( expr__, message__ ) \
+      fprintf(stderr, ::std::format("Assertion Failed ({}:{}): {} - ({} is wrong)\n", __FILE__, __LINE__, message__, #expr__ ).c_str())
+# else
+#   include <cstdlib>
+#   include <cstdio>
+#   define NOCONTRACTS_LOG_FAILURE( expr__, message__ ) \
+      ::fprintf(stderr, "Assertion Failed (%s:%i): %s - (%s is wrong)\n", __FILE__, __LINE__, message__, #expr__ )
+# endif
 #endif
 
 #ifndef NOCONTRACTS_ABORT
 # define NOCONTRACTS_ABORT() \
-    std::abort();
+    std::abort()
 #endif
 
 #ifndef NOCONTRACTS_DEBUGBREAK
